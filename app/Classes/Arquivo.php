8c43e9df6cd28b2d334259d86e9a04f3991c88b7
alerrandro borges <alerrandrokaton@gmail.com>
2022-11-07 21:07:37 -0300

    Feat: tratamento de erros

--- app/Classes/Arquivo.php
+++ app/Classes/Arquivo.php
@@ -4,12 +4,46 @@ namespace App\Classes;
 
 class Arquivo
 {
-    public static function transferenciaDeArquivo($caminhoAtual, $caminhoNovo)
+    public static function transferenciaDeArquivo($fileError, $caminhoAtual, $caminhoNovo)
     {
-        if (move_uploaded_file($caminhoAtual, $caminhoNovo)) {
-            echo 'Arquivo movido';
-        }else{
-            echo 'Erro';
+        self::erros($fileError);
+        move_uploaded_file($caminhoAtual, $caminhoNovo);
+    }
+
+    public static function erros($fileError)
+    {
+        switch ($fileError) {
+            case '0':
+                echo "<span class='sucesso'>Arquivo movido com sucesso</span>";
+                break;
+            case '1':
+                echo "<span class='erro'> Error: Mova apenas um arquivo</span>";
+                exit;
+                break;
+            case '2':
+                echo "<span class='erro'>Error: Arquivo muito grande</span>";
+                exit;
+                break;
+            case '3':
+                echo "<span class='erro'>Error: O upload do arquivo foi feito parcialmente.</span>";
+                exit;
+                break;
+            case '4':
+                echo "<span class='erro'>Error: Nenhum arquivo foi enviado</span>";
+                exit;
+                break;
+            case '6':
+                echo "<span class='erro'>Error: Pasta temporária ausente</span>";
+                exit;
+                break;
+            case '7':
+                echo "<span class='erro'>Error: Falha ao escrever o arquivo no disco</span>";
+                exit;
+                break;
+            case '8':
+                echo "<span class='erro'>Error: Uma extensão do PHP interrompeu o upload do arquivo.</span>";
+                exit;
+                break;
         }
     }
 }
--- arquivo.php
+++ arquivo.php
@@ -3,27 +3,78 @@ require __DIR__ . '/vendor/autoload.php';
 
 use App\Classes\Arquivo;
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transferençia de Arquivos</title>
-</head>
-<body>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="arquivo">
-        <input type="submit" value="Enviar">
-    </form>
 
-    <?php
-        if (isset($_FILES['arquivo'])) {
-            $nomeArquivo = $_FILES['arquivo']['name'];
-            $caminhoAtual = $_FILES['arquivo']['tmp_name'];
-            $caminhoNovo = 'imagens/'.$nomeArquivo;
-            Arquivo::transferenciaDeArquivo($caminhoAtual, $caminhoNovo);
-        }
-    ?>
-</body>
-</html>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .container {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .formulario {
+        text-align: center;
+    }
+
+    input {
+        display: block;
+        margin: 10px;
+    }
+
+    input[type='submit'] {
+        padding: 5px;
+        width: 280px;
+        border: 0;
+        cursor: pointer;
+        font-size: 15px;
+        background-color: #222222;
+        color: #fff;
+
+    }
+
+    .sucesso{
+        background-color: green;
+        padding: 10px;
+        font-size: 15px;
+        color: #fff;
+    }
+
+    .erro{
+        background-color: red;
+        padding: 10px;
+        font-size: 15px;
+        color: #fff;
+    }
+
+    .erro-container{
+        margin: 15px 0;
+    }
+</style>
+
+<title>Transferençia de Arquivos</title>
+<section class="container">
+    <div class="formulario">
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="file" name="arquivo">
+            <input type="submit" value="Enviar">
+        </form>
+        <div class="erro-container">
+            <?php
+            if (isset($_FILES['arquivo'])) {
+                $fileError = $_FILES['arquivo']['error'];
+                $nomeArquivo = $_FILES['arquivo']['name'];
+                $caminhoAtual = $_FILES['arquivo']['tmp_name'];
+                $caminhoNovo = 'imagens/' . $nomeArquivo;
+                Arquivo::transferenciaDeArquivo($fileError, $caminhoAtual, $caminhoNovo);
+            }
+            ?>
+        </div>
+    </div>
+</section>
